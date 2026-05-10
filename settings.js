10b7cb4494da33b701d5118d0db1307f06eb2c43
nay <150757032+naylafitiaarwani@users.noreply.github.com>
2026-05-10 18:09:10 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,19 +1,5 @@
 module.exports = {
     uiPort: process.env.PORT || 1880,
-
-    mqttReconnectTime: 15000,
-
-    serialReconnectTime: 15000,
-
-    debugMaxLength: 1000,
-
     flowFile: 'flows.json',
-
-    credentialSecret: false,
-
-    editorTheme: {
-        projects: {
-            enabled: false
-        }
-    }
+    credentialSecret: false
 }
